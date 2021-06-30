21b747881d62d80b6b4e66e9e6043375ad6a31a7
Diego <diegoteixeirapereira@hotmail.com>
2021-06-30 17:27:57 -0300

    criação do arquivo java de conexão com banco.

---
+++ JavaCrudConsoleEcommerce/src/br/com/java/db/ConnectionFactory.java
@@ -0,0 +1,38 @@
+package br.com.java.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+	
+	public static Connection getConnection() throws SQLException {
+		
+		Connection con = null;
+		
+		try {
+			String servidorNome= "localhost";
+			String bancoDeDadosNome = "dbJavaCrudConsoleEcommerce";
+			String usuario = "root";
+			String senha = "root";
+			String driverNome = "com.mysql.jdbc.Driver";
+			Class.forName(driverNome);
+			
+			String url = "jdbc:mysql://" + servidorNome + "/" + bancoDeDadosNome;
+			
+			con = DriverManager.getConnection(url, usuario, senha);
+			
+			System.out.println("Conexão realizada com sucesso!.");
+		} catch (ClassNotFoundException e) {
+			// TODO: handle exception
+			System.out.println("Driver não encontrado" + e.toString());
+		}catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("Erro ao conectar o banco de dados " + e.toString());
+		}
+		return con;
+	}
+	
+	
+	
+}
